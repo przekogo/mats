205824c10f4ccc50e1d4faf2cc9eb5ebd9bfe32e
Przemek Rogos <przemyslaw.rogos@netguru.pl>
2017-05-31 11:46:41 +0200

    query object

--- ArticlesQuery.rb
+++ ArticlesQuery.rb
@@ -5,22 +5,18 @@ class ArticlesQuery
     @relation = relation
   end
 
-  def published
-    relation.where(published: true)
-  end
-
-  def minimal_view_count(view_count)
-    return relation unless view_count.present?
-    relation.where('view_count > ?', view_count)
-  end
-
-  def author_first_name_like(first_name)
-    return relation unless first_name.present?
-    with_authors
-      .where('users.first_name LIKE ?', "#{first_name}%")
+  def by_author_with_viewcount(view_count, first_name)
+    by_author_with_viewcount_query
   end
 
   private
+
+  def by_author_with_viewcount_query
+    with_authors
+      .where(published: true)
+      .where('view_count > ?', view_count)
+      .where('users.first_name LIKE ?', "#{first_name}%")
+  end
 
   def with_authors
     relation.joins('LEFT OUTER JOIN users ON users.id = articles.author_id')
